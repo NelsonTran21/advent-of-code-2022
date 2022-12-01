22aabace988898f68fd2d057d67573754e1a60fc
Nelson Tran <nelson@tran21.com>
2022-11-30 23:02:30 -0700

    Refactor Day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,31 +1,24 @@
 type Calories = u32;
 
-fn parse_input() -> Vec<Vec<Calories>> {
+fn parse_input() -> Vec<Calories> {
    include_str!("../input/day01.txt")
       .split("\n\n")
       .map(|food_bags| {
          food_bags
             .lines()
-            .map(|calories| calories.parse().expect("invalid calorie value"))
-            .collect()
+            .map(|food| food.parse::<Calories>().expect("bad value"))
+            .sum()
       })
       .collect()
 }
 
 pub fn solve_part1() -> Calories {
-   parse_input()
-      .iter()
-      .map(|food_bag| food_bag.iter().sum())
-      .max()
-      .expect("no food bags")
+   let food_bags = parse_input();
+   food_bags.into_iter().max().expect("no food bags")
 }
 
 pub fn solve_part2() -> Calories {
-   let mut food_bags: Vec<Calories> = parse_input()
-      .iter()
-      .map(|food_bag| food_bag.iter().sum())
-      .collect();
-
+   let mut food_bags = parse_input();
    food_bags.sort_by(|a, b| b.cmp(a));
-   food_bags.iter().take(3).sum()
+   food_bags.into_iter().take(3).sum()
 }
