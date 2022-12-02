b717cd93ee4ea108f7e87a308c53f5958f5445f3
Nelson Tran <nelson@tran21.com>
2022-12-01 23:05:47 -0700

    Refactor Day02

--- src/day02.rs
+++ src/day02.rs
@@ -1,36 +1,110 @@
-#[derive(Debug, Copy, Clone, PartialEq)]
+enum Part {
+   One,
+   Two,
+}
+
+#[derive(Copy, Clone, PartialEq)]
 enum Shape {
    Rock = 1,
    Paper = 2,
    Scissors = 3,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Copy, Clone)]
 enum Outcome {
    Loss = 0,
    Draw = 3,
    Win = 6,
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Game(Shape, Shape);
-
-#[derive(Debug, Clone, Copy)]
-struct AltGame(Shape, Outcome);
-
 type Score = u32;
 
+#[derive(Copy, Clone)]
+struct Game(Shape, Shape);
+
+#[derive(Copy, Clone)]
+struct Strategy(Shape, Outcome);
+
+impl Into<Game> for Strategy {
+   fn into(self) -> Game {
+      use Outcome::*;
+      use Shape::*;
+
+      let player_shape = match self {
+         Strategy(Rock, Loss) => Scissors,
+         Strategy(Rock, Draw) => Rock,
+         Strategy(Rock, Win) => Paper,
+
+         Strategy(Paper, Loss) => Rock,
+         Strategy(Paper, Draw) => Paper,
+         Strategy(Paper, Win) => Scissors,
+
+         Strategy(Scissors, Loss) => Paper,
+         Strategy(Scissors, Draw) => Scissors,
+         Strategy(Scissors, Win) => Rock,
+      };
+
+      Game(self.0, player_shape)
+   }
+}
+
+fn parse_shape(letter: &'static str) -> Shape {
+   use Shape::*;
+
+   match letter {
+      "A" | "X" => Rock,
+      "B" | "Y" => Paper,
+      "C" | "Z" => Scissors,
+
+      _ => unreachable!(),
+   }
+}
+
+fn parse_outcome(letter: &'static str) -> Outcome {
+   use Outcome::*;
+
+   match letter {
+      "X" => Loss,
+      "Y" => Draw,
+      "Z" => Win,
+
+      _ => unreachable!(),
+   }
+}
+
+fn parse_input(part: Part) -> Vec<Game> {
+   include_str!("../input/day02.txt")
+      .lines()
+      .map(|line| {
+         let tokens: Vec<&'static str> = line.split(" ").collect();
+         let opponent = parse_shape(tokens[0]);
+         let player = parse_shape(tokens[1]);
+         let outcome = parse_outcome(tokens[1]);
+
+         match part {
+            Part::One => Game(opponent, player),
+            Part::Two => Strategy(opponent, outcome).into(),
+         }
+      })
+      .collect()
+}
+
 fn get_outcome(game: Game) -> Outcome {
+   use Outcome::*;
+   use Shape::*;
+
    match game {
-      Game(Shape::Rock, Shape::Rock) => Outcome::Draw,
-      Game(Shape::Rock, Shape::Paper) => Outcome::Win,
-      Game(Shape::Rock, Shape::Scissors) => Outcome::Loss,
-      Game(Shape::Paper, Shape::Rock) => Outcome::Loss,
-      Game(Shape::Paper, Shape::Paper) => Outcome::Draw,
-      Game(Shape::Paper, Shape::Scissors) => Outcome::Win,
-      Game(Shape::Scissors, Shape::Rock) => Outcome::Win,
-      Game(Shape::Scissors, Shape::Paper) => Outcome::Loss,
-      Game(Shape::Scissors, Shape::Scissors) => Outcome::Draw,
+      Game(Rock, Rock) => Draw,
+      Game(Rock, Paper) => Win,
+      Game(Rock, Scissors) => Loss,
+
+      Game(Paper, Rock) => Loss,
+      Game(Paper, Paper) => Draw,
+      Game(Paper, Scissors) => Win,
+
+      Game(Scissors, Rock) => Win,
+      Game(Scissors, Paper) => Loss,
+      Game(Scissors, Scissors) => Draw,
    }
 }
 
@@ -38,69 +112,10 @@ fn evaluate(game: Game) -> Score {
    (game.1 as Score) + (get_outcome(game) as Score)
 }
 
-fn convert_game(game: AltGame) -> Game {
-   let player_move = match game {
-      AltGame(Shape::Rock, Outcome::Loss) => Shape::Scissors,
-      AltGame(Shape::Rock, Outcome::Draw) => Shape::Rock,
-      AltGame(Shape::Rock, Outcome::Win) => Shape::Paper,
-      AltGame(Shape::Paper, Outcome::Loss) => Shape::Rock,
-      AltGame(Shape::Paper, Outcome::Draw) => Shape::Paper,
-      AltGame(Shape::Paper, Outcome::Win) => Shape::Scissors,
-      AltGame(Shape::Scissors, Outcome::Loss) => Shape::Paper,
-      AltGame(Shape::Scissors, Outcome::Draw) => Shape::Scissors,
-      AltGame(Shape::Scissors, Outcome::Win) => Shape::Rock,
-   };
-
-   Game(game.0, player_move)
-}
-
-fn parse_move(letter: &'static str) -> Shape {
-   match letter {
-      "A" => Shape::Rock,
-      "B" => Shape::Paper,
-      "C" => Shape::Scissors,
-      "X" => Shape::Rock,
-      "Y" => Shape::Paper,
-      "Z" => Shape::Scissors,
-      _ => unreachable!(),
-   }
-}
-
-fn parse_outcome(letter: &'static str) -> Outcome {
-   match letter {
-      "X" => Outcome::Loss,
-      "Y" => Outcome::Draw,
-      "Z" => Outcome::Win,
-      _ => unreachable!(),
-   }
-}
-
-fn parse_part_one_input() -> Vec<Game> {
-   include_str!("../input/day02.txt")
-      .lines()
-      .map(|line| {
-         let moves: Vec<&'static str> = line.split(" ").collect();
-         Game(parse_move(moves[0]), parse_move(moves[1]))
-      })
-      .collect()
-}
-
-fn parse_part_two_input() -> Vec<AltGame> {
-   include_str!("../input/day02.iq.txt")
-      .lines()
-      .map(|line| {
-         let moves: Vec<&'static str> = line.split(" ").collect();
-         AltGame(parse_move(moves[0]), parse_outcome(moves[1]))
-      })
-      .collect()
-}
-
 pub fn solve_part_one() -> Score {
-   let games = parse_part_one_input();
-   games.into_iter().map(evaluate).sum()
+   parse_input(Part::One).into_iter().map(evaluate).sum()
 }
 
 pub fn solve_part_two() -> Score {
-   let games = parse_part_two_input();
-   games.into_iter().map(convert_game).map(evaluate).sum()
+   parse_input(Part::Two).into_iter().map(evaluate).sum()
 }
