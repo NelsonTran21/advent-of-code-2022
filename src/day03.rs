6430d47920c57fd89500b81d413df393bd176963
Nelson Tran <nelson@tran21.com>
2022-12-02 23:25:31 -0700

    Refactor Day03

--- src/day03.rs
+++ src/day03.rs
@@ -4,16 +4,21 @@ type Item = char;
 
 type Priority = u32;
 
-#[derive(Debug)]
 struct Rucksack(HashSet<Item>, HashSet<Item>);
+
+impl Rucksack {
+   fn all_items(&self) -> HashSet<Item> {
+      self.0.union(&self.1).copied().collect()
+   }
+}
 
 fn parse_input() -> Vec<Rucksack> {
    include_str!("../input/day03.txt")
       .lines()
       .map(|line| {
-         let length = line.chars().count();
-         let (first, last) = line.split_at(length / 2);
-         Rucksack(first.chars().collect(), last.chars().collect())
+         let rucksack_size = line.chars().count();
+         let (first, second) = line.split_at(rucksack_size / 2);
+         Rucksack(first.chars().collect(), second.chars().collect())
       })
       .collect()
 }
@@ -22,48 +27,28 @@ fn get_priority(item: Item) -> Priority {
    match item {
       'a'..='z' => u32::from(item) - 97 + 1,
       'A'..='Z' => u32::from(item) - 65 + 27,
-      _ => {
-         println!("{:?}", item);
-         unreachable!()
-      }
+      _ => unreachable!(),
    }
 }
 
-fn find_matching_items_in_rucksack(rucksack: Rucksack) -> HashSet<Item> {
-   rucksack
-      .0
-      .intersection(&rucksack.1)
-      .map(|item| item.to_owned())
-      .collect()
+fn find_matching_item(rucksack: Rucksack) -> HashSet<Item> {
+   rucksack.0.intersection(&rucksack.1).copied().collect()
 }
 
-fn find_matching_items_in_rucksacks(
-   one: &Rucksack,
-   two: &Rucksack,
-   three: &Rucksack,
-) -> HashSet<Item> {
-   let all_one: HashSet<Item> = one.0.union(&one.1).into_iter().copied().collect();
-   let all_two: HashSet<Item> = two.0.union(&two.1).into_iter().copied().collect();
-   let all_three: HashSet<Item> = three.0.union(&three.1).into_iter().copied().collect();
-
-   all_one
-      .intersection(&all_two)
-      .into_iter()
-      .copied()
-      .collect::<HashSet<_>>()
-      .intersection(&all_three)
-      .into_iter()
-      .copied()
-      .collect()
+fn find_group_badge(rucksacks: &[Rucksack]) -> HashSet<Item> {
+   rucksacks
+      .iter()
+      .skip(1)
+      .fold(rucksacks[0].all_items(), |acc, rucksack| {
+         acc.intersection(&rucksack.all_items()).copied().collect()
+      })
 }
 
 pub fn solve_part_one() -> Priority {
-   let rucksacks = parse_input();
-
-   rucksacks
+   parse_input()
       .into_iter()
       .map(|rucksack| -> Priority {
-         find_matching_items_in_rucksack(rucksack)
+         find_matching_item(rucksack)
             .into_iter()
             .map(get_priority)
             .sum()
@@ -72,12 +57,11 @@ pub fn solve_part_one() -> Priority {
 }
 
 pub fn solve_part_two() -> Priority {
-   let rucksacks = parse_input();
-   let rucksack_chunks: Vec<&[Rucksack]> = rucksacks.chunks(3).collect();
-   rucksack_chunks
+   parse_input()
+      .chunks(3)
       .into_iter()
       .map(|rucksacks| -> Priority {
-         find_matching_items_in_rucksacks(&rucksacks[0], &rucksacks[1], &rucksacks[2])
+         find_group_badge(rucksacks)
             .into_iter()
             .map(get_priority)
             .sum()
